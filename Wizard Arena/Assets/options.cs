03d0ab2d85f3e8d1f3c4dc171e0cf8993beffa4c
Demospengu <demospenguin@gmail.com>
2025-05-12 09:04:30 -0500

    stuff

--- Wizard Arena/Assets/options.cs
+++ Wizard Arena/Assets/options.cs
@@ -3,21 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Startbutton : MonoBehaviour
+public class options : MonoBehaviour
 {
+    public GameObject OptionsMenu;
     // Start is called before the first frame update
     void Start()
     {
-        
+        OptionsMenu.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-       
+        if (Input.GetKey(KeyCode.K))
+        {
+            OptionsMenu.SetActive(false);
+        }
     }
+
     private void OnMouseDown()
     {
-        SceneManager.LoadScene("Level1");
+        OptionsMenu.SetActive(true);
+
     }
 }
